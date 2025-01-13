0b86111e653049511ceec1a45dc653ba25e58c30
kwang <chlrhkdtjq11@naver.com>
2025-01-13 11:34:15 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/org/example/workspace/ApiArtistWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package org.example.workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiArtistWorkspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiArtistWorkspaceApplication.class, args);
+    }
+
+}
