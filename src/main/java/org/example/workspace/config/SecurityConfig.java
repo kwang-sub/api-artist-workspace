3fa9b0783af1cb4bf007323295cba84f32ae338a
kwang <chlrhkdtjq11@naver.com>
2025-01-13 12:15:56 +0900

    feat: cors 기본 설정 추가

---
+++ src/main/java/org/example/workspace/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package org.example.workspace.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("/api/v1/login").permitAll()
+                                .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
---
+++ src/main/java/org/example/workspace/controller/LoginController.java
@@ -0,0 +1,19 @@
+package org.example.workspace.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class LoginController {
+
+    @GetMapping("/login")
+    public ResponseEntity<Object> login() {
+
+        return ResponseEntity.ok().build();
+    }
+}
