9005757bfbc6dd2972713d4ef72d05cd638d4662
kwang <chlrhkdtjq11@naver.com>
2025-01-21 11:22:21 +0900

    test: 사용자 계정 지원 테스트 추가

--- src/test/java/org/example/workspace/integration/domain/user/UsersSupportTest.java
+++ src/test/java/org/example/workspace/integration/domain/user/UsersSupportTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -68,5 +69,35 @@ public class UsersSupportTest {
         ProblemDetail response = objectMapper.readValue(responseString, ProblemDetail.class);
 
         assertThat(response.getDetail()).isEqualTo("유효하지 않은 토큰 정보입니다.");
+    }
+
+    @Test
+    void 사용자는_이메일로_마스킹된_아이디를_찾을수있다() {
+        fail();
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 사용자는_로그인아이디_이메일로_임시_비밀번호를_이메일로_발송할수있다() {
+        fail();
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void 사용자_비밀번호_찾기시_로그인아이디_이메일에_맞는_계정_없는_경우_예외발생한다() {
+        fail();
+        // given
+
+        // when
+
+        // then
     }
 }
